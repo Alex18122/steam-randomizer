c10cee701ff3ae2007ad52cc79085b339859a183
Gabinho <gbenincasa042@gmail.com>
2026-06-12 14:31:56 -0300

    feat(library) : fetch and display game list from steam api

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-steam_id = input("ingrese su ide de steam")
+steam_id = "76561198867346433"
 
 url = (f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&steamid={steam_id}&include_appinfo=1&format=json")
+
+response = requests.get(url)
+data = response.json()
+
+games = data["response"]["games"]
+
+for game in sorted(games, key=lambda g: g["name"]):
+    print(f"{game["name"]} appId: {game["appid"]} playtime : {game["playtime_forever"]}")
