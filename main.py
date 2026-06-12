a6f3b983811dcda68ea0dce21cb2541aadf0cefa
Gabinho <gbenincasa042@gmail.com>
2026-06-12 13:29:26 -0300

    remove api key, add .env

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-API_KEY = "E5A8D84A5EC9F9BC368FC36DC6365CAF"
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
 steam_id = input("ingrese su ide de steam")
 
 url = (f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={API_KEY}&steamid={steam_id}&include_appinfo=1&format=json")
